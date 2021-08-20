b4fc2907ef5db0364c4aa821b2edb0014ee4504d
EC2 Default User <ec2-user@ip-172-31-5-17.ec2.internal>
2021-08-20 17:56:17 +0000

    Lab2, not complete

---
+++ Lab2/Chat_Client.py
@@ -0,0 +1,50 @@
+import http.client
+import sys
+
+if len(sys.argv) != 3:
+    print ("How to make it work: script, IP address, PORT number")
+    exit()
+
+# The first argument from the prompt is saved in IP_ADDRESS as a IP address.
+IP_ADDRESS = str(sys.argv[1])
+
+# The second argument from the prompt is saved in PORT as a port number.
+PORT = int(sys.argv[2])
+
+conn = http.client.HTTPConnection(IP_ADDRESS, PORT)
+
+while True:
+    while True:
+        n_name = input("Nickname: ")
+        if len(n_name > 0):
+            break
+        else:
+            print("Please enter a Nickname/Username")
+
+    conn.request('POST',"/nickname", n_name.encode())
+    r = conn.getresponse()
+    if (r.status == 201):
+        break
+    else:
+        print("Nickname already taken, try another one")
+
+while True:
+    try:
+        while True:
+            m = input("<{}>".format(n_name))
+            if len(m) != 0:
+                m = n_name + ">" + m
+                conn.request("POST","/",m.encode())
+                r = conn.getresponse()
+
+            conn.request("POST","/getM",n_name.encode())
+            r = conn.getresponse()
+            d = r.read().decode()
+            print(d[2:])
+
+    except KeyboardInterrupt as error:
+        print("Keyboard Interruption... bye bye: {}".format(str(error)))
+        conn.close()
+        sys.exit()
+
+conn.close()
---
+++ Lab2/Chat_Server.py
@@ -0,0 +1,76 @@
+from  http.server import HTTPServer, BaseHTTPRequestHandler
+import select
+import sys
+
+# Checks if the number of arguments is given is correct.
+if len(sys.argv) != 3:
+    print ("Correct usage: script, IP address, port number")
+    exit()
+
+# The first argument from the prompt is saved in IP_ADDRESS as a IP address.
+IP_ADDRESS = str(sys.argv[1])
+
+# The second argument from the prompt is saved in PORT as a port number.
+PORT = int(sys.argv[2])
+
+msgs={}
+
+class message_Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+        n_name = ""
+        m = msgs.get(n_name)
+        msgs[n_name] = ""
+        msg = "nada"
+        self.wfile.write(msg.encode())
+
+    def do_POST(self):
+        if(self.path.endswith("/nickname")):
+            content_len = int(self.headers.get('Content-Length'))
+            post_body = self.rfile.read(content_len)
+            post_body = post_body.decode()
+            if msgs.get(post_body) == None:
+                msgs[post_body] = ""
+                self.send_response(201)
+                self.send_header('content-type','text/html')
+                self.end_headers()
+                print("Nickname registered " + post_body)
+            else:
+                self.send_response(401)
+                self.send_header('content-type','text/html')
+                self.end_headers()
+        elif(self.path.endswith("/getM")):
+            content_len = int(self.headers.get('Content-Length'))
+            post_body = self.rfile.read(content_len).decode()
+            self.send_response(200)
+            self.send_header('content-type','text/html')
+            self.end_headers()
+            message = msgs.get(post_body)
+            msgs[post_body] = ""
+            self.wfile.write(message.encode())
+
+        else:
+            content_len = int(self.headers.get('Content-Length'))
+            post_body = self.rfile.read(content_len)
+            post_body = post_body.decode()
+            n_name = post_body.split(">")[0]
+            for name in msgs:
+                if name != n_name:
+                    msg = msgs.get(name)
+                    msg = msg + "\n" + post_body
+                    msgs[name] = msg
+            self.send_response(200)
+            self.send_header('content-type','text/html')
+            self.end_headers()
+            # self.wfile.write('POST'.encode())
+
+
+def main():
+    server = HTTPServer((IP,PORT),msgHandler)
+    print("Server running on port {}".format(PORT))
+    server.serve_forever()
+
+if __name__ == "__main__":
+    main()
