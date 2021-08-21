fe246913e8776fe8770ad863c22dfcc62b46206b
EC2 Default User <ec2-user@ip-172-31-3-69.ec2.internal>
2021-08-21 22:12:39 +0000

    Changes

--- Lab2/Chat_Client.py
+++ Lab2/Chat_Client.py
@@ -29,31 +29,19 @@ while True:
         print("Nickname already taken, try another one")
 
 while True:
-    try:
-        m = sys.stdin.readline()
-        if (len(m) != 0):
-            m = "<" + n_name + "> " + m
-            conn.request("POST","/",m.encode())
-            r = conn.getresponse()
-            
-        else:
+    try:    
+        while True:
+            m = input("<{}> ".format(n_name))
+            print("m: ", m)
+            if len(m) != 0:
+                m = "<" + n_name + "> " + m
+                conn.request("POST","/",m.encode())
+                r = conn.getresponse()
+
             conn.request("POST","/getM",n_name.encode())
             r = conn.getresponse()
             d = r.read().decode()
             print("Response received: ", d)
-            
-        #while True:
-            #m = input("<{}> ".format(n_name))
-            #print("m: ", m)
-            #if len(m) != 0:
-                #m = "<" + n_name + "> " + m
-                #conn.request("POST","/",m.encode())
-                #r = conn.getresponse()
-
-            #conn.request("POST","/getM",n_name.encode())
-            #r = conn.getresponse()
-            #d = r.read().decode()
-            #print("Response received: ", d)
 
     except KeyboardInterrupt as error:
         print("Keyboard Interruption... bye bye: {}".format(str(error)))
