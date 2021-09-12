34a3b0a7ea37ae29309b3ed2ea66c2ce5e4db609
EC2 Default User <ec2-user@ip-172-31-17-61.ec2.internal>
2021-09-12 21:53:02 +0000

    Lab4

--- Lab4/appConsumer.py
+++ Lab4/appConsumer.py
@@ -1,10 +1,32 @@
 import pika
+import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('52.203.182.145', 5672, '/', pika.PlainCredentials("user", "password")))
+
+if len(sys.argv) != 5:
+    print ("Correct usage: script, IP address, PORT number, RabbitMQ User and Password")
+    exit()
+else:
+    IP = str(sys.argv[1])
+    PORT = str(sys.argv[2])
+    user = str(sys.argv[3])
+    pswd = str(sys.argv[4])
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(IP, PORT, '/', pika.PlainCredentials(user, pswd)))
 channel = connection.channel()
 
 def callback(ch, method, properties, body):
-    print(f'{body} is received')
-    
+    print(f'{body.decode()} is received')
+
+
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue="my_app", on_message_callback=callback, auto_ack=True)
-channel.start_consuming()
+
+
+while True:
+    try:
+        channel.start_consuming()
+
+    except KeyboardInterrupt:
+        print('Keyboard Interrupt bye bye... Shutting Down')
+        channel.stop_consuming
+        sys.exit()
--- Lab4/appProducer.py
+++ Lab4/appProducer.py
@@ -1,11 +1,32 @@
 import pika
+import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('52.203.182.145', 5672, '/', pika.PlainCredentials('user', 'password')))
+if len(sys.argv) != 5:
+    print ("Correct usage: script, IP address, PORT number, RabbitMQ User and Password")
+    exit()
+else:
+    IP = str(sys.argv[1])
+    PORT = str(sys.argv[2])
+    user = str(sys.argv[3])
+    pswd = str(sys.argv[4])
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(IP, PORT, '/', pika.PlainCredentials(user, pswd)))
 channel = connection.channel()
 
-channel.basic_publish(exchange='my_exchange', routing_key='test', body='Test!')
+t = ''
 
 print("Runnning Producer Application...")
-print(" [x] Sent 'Hello World...!'")
+print("Type task to be send")
 
-connection.close()
+
+while True:
+    try:
+        t = sys.stdin.readline()
+        if (t != ''):
+            channel.basic_publish(exchange='my_exchange', routing_key='test', body=t)
+            print('{t} taks send succesfully!')
+            
+    except KeyboardInterrupt:
+        print('Keyboard Interrupt bye bye... Shutting Down')
+        connection.close()
+        sys.exit()
